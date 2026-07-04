b11b6820fb4b6a666fc54bf557008cc5a8bdafb5
Isaac Santana <isaac-santana@outlook.com>
2026-07-04 09:28:08 -0300

    add dynamic sitemap

--- worker.js
+++ worker.js
@@ -3,6 +3,48 @@
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const origin = url.origin;
+
+    if (url.pathname === "/sitemap.xml") {
+      const lastmod = "2026-07-04";
+      const urls = [
+        { loc: `${origin}/`, priority: "1.0" },
+        { loc: `${origin}/byinspare/`, priority: "0.8" },
+      ];
+      const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((item) => `  <url>
+    <loc>${item.loc}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${item.priority}</priority>
+  </url>`).join("\n")}
+</urlset>
+`;
+      return new Response(body, {
+        headers: {
+          "content-type": "application/xml; charset=UTF-8",
+          "cache-control": "public, max-age=3600",
+        },
+      });
+    }
+
+    if (url.pathname === "/robots.txt") {
+      const body = `User-agent: *
+Allow: /
+Disallow: /chassis/
+
+Sitemap: ${origin}/sitemap.xml
+`;
+      return new Response(body, {
+        headers: {
+          "content-type": "text/plain; charset=UTF-8",
+          "cache-control": "public, max-age=3600",
+        },
+      });
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
